9316ec63c3808980be76ed6e7931c6ce8a851dc0
boosey <boosey.boudreaux@gmail.com>
2018-12-15 11:00:11 -0600

    Added ability to edit a game

--- src/components/app-commands/app-commands.tsx
+++ src/components/app-commands/app-commands.tsx
@@ -1,5 +1,8 @@
 
 import { Component, Event, EventEmitter, Listen } from '@stencil/core';
+import { docData } from 'rxfire/firestore';
+import {first} from 'rxjs/operators'
+
 
 declare var firebase: firebase.app.App;
 
@@ -13,6 +16,25 @@ export class AppCommands {
 
     @Event() addGameCompleted: EventEmitter
     @Event() editGameCompleted: EventEmitter
+
+    private gamesCollectionRef = firebase.firestore().collection('games')
+
+    @Listen('loadGameRequested')
+    loadGameRequestedHandler(ev){
+      var gameId = ev.detail.data
+      var status = ev.detail.status
+      var gameRef = this.gamesCollectionRef.doc(gameId)
+
+      docData(gameRef).pipe(first()).subscribe((doc) => status.next(doc))
+    }
+
+    @Listen('updateGameRequested')
+    updateGameRequestHandler(ev) {
+      var gameId = ev.detail.gameId
+      this.gamesCollectionRef.doc(gameId)
+        .update(ev.detail.gameData)
+        .then(ev.detail.status.complete())
+    }
 
     @Listen('addGameRequested')
     addGameRequestedHandler(ev) {
--- src/components/app-edit-game/app-edit-game.tsx
+++ src/components/app-edit-game/app-edit-game.tsx
@@ -1,14 +1,75 @@
 
-import { Component, Event, EventEmitter } from '@stencil/core';
+import { Component, Event, EventEmitter, Listen, Prop, State } from '@stencil/core';
+import { Subject } from "rxjs";
+import { first } from 'rxjs/operators';
+
+const nav = document.querySelector('ion-nav')
 
 @Component({
     tag: 'app-edit-game',
     styleUrl: 'app-edit-game.css'
 })
-export class AppAddGame {
 
-    @Event() editGameRequested: EventEmitter
-    @Event() editGameCompleted: EventEmitter
+export class AppEditGame {
+
+    @Prop() gameId: string
+    @State() game = null;
+
+    @Event() loadGameRequested: EventEmitter
+    @Event() updateGameRequested: EventEmitter
+
+      // game = {name: 'Default Game'}
+
+    componentWillLoad() {
+      // Load game from ID passed in in the URL path
+      var requestStatus = (new Subject())
+      .pipe(first())
+      .subscribe((game) => this.game = game)
+
+      var request = { data: this.gameId, status: requestStatus}
+      this.loadGameRequested.emit(request)
+    }
+
+    // Listen for submission from Details pane then call command
+    // @Listen('gameDetailsSubmitted')
+    // requestUpdateGame(ev) {
+    //   var requestStatus = new Subject()
+    //   var requestSubscription = requestStatus.subscribe(
+    //     (gameRef) => (null),
+    //     () => console.log("Error updating game"),
+    //     () => nav.pop()
+    //   )
+    //   var request = { data: ev.data, status: requestStatus}
+    //   this.updateGameRequested.emit(request)
+    // }
+
+    // This is the local function for calling the command
+    requestUpdateGame() {
+      var requestStatus = (new Subject())
+      .subscribe(
+        () => (null),
+        () => console.log("Error updating game"),
+        () => nav.pop()
+      )
+      var request = {
+        gameId: this.gameId,
+        gameData: this.game,
+        status: requestStatus
+      }
+      this.updateGameRequested.emit(request)
+    }
+
+
+    changeValue(ev){
+        let value = ev.target.value;
+        switch(ev.target.name){
+
+            case 'name': {
+                this.game.name = value;
+                break;
+            }
+        }
+    }
 
     render() {
         return [
@@ -24,6 +85,16 @@ export class AppAddGame {
           </ion-header>,
 
           <ion-content padding>
+          <ion-list>
+              <ion-item>
+                  <ion-label position="floating">Name</ion-label>
+                  <ion-input name="name" value={this.game ? this.game.name : ''}
+                    onInput={(ev) => this.changeValue(ev)} type="text"></ion-input>
+              </ion-item>
+          </ion-list>
+
+          <ion-button onClick={() => this.requestUpdateGame()}>Save Changes</ion-button>
+
           </ion-content>
         ]
     }
